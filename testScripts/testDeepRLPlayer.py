2c4cf3cfe07df4d972c5a631e31102d1c8d294f3
masus04 <thomas_st@hotmail.com>
2017-09-06 21:35:24 +0200

    Implemented basic MCPlayer

--- testScripts/testDeepRLPlayer.py
+++ testScripts/testDeepRLPlayer.py
@@ -1,9 +1,10 @@
 import config
 from board import Board
-from player import DeepRLPlayer
+from player import MCPlayer
 from valueFunction import ValueFunction
 
 board = Board()
-player = DeepRLPlayer(color=config.BLACK, time_limit=config.TIMEOUT, strategy=ValueFunction())
+player = MCPlayer(color=config.BLACK, time_limit=config.TIMEOUT, strategy=ValueFunction())
 
 print(player.get_move(board))
+player.save_params()
