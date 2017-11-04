3044b7662fa42f569e46a53cbf687a89a31b6467
masus04 <thomas_st@hotmail.com>
2017-11-04 14:17:14 +0100

    minor improvements

--- continuousTraining.py
+++ continuousTraining.py
@@ -6,26 +6,29 @@ from evaluation import compare_players
 from src.player import MCPlayer, TDPlayer
 from src.valueFunction import ValueFunction
 
-EXPERIMENT_NAME = "|continuous|"
+EXPERIMENT_NAME = "|Continuous Training|"
 
 
-def train_continuous(player1, player2, iterations):
+def train_continuous(player1, player2, games, evaluation_period, experiment_name, iterations):
+    """Trains a pair of players for @games games, evaluating them every @evaluation_period games and repeats the process with the stronger of the two players for @iterations iterations"""
 
     for i in range(iterations):
-        train(player1, player2, GAMES_PER_ITERATION, EVALUATION_PERIOD, EXPERIMENT_NAME)
-        player1, player2 = (player1, player2) if compare_players(player1, player2, silent=(i == iterations-1)) >= 0 else (player2, player1)
-        player2.value_function = player1.value_function.copy(player2.plotter)
+        train(player1, player2, games, evaluation_period, experiment_name)
+        player1, player2 = (player1, player2) if compare_players(player1, player2, silent=(i != iterations-1)) >= 0 else (player2, player1)
+        player2.value_function = player1.value_function.copy()
+
+    return player1, player2
 
 
 if __name__ == "__main__":
 
     """ Parameters """
     PLAYER = TDPlayer
-    ITERATIONS = 2
-    GAMES_PER_ITERATION = 50
-    EVALUATION_PERIOD = 25
+    ITERATIONS = 10
+    GAMES_PER_ITERATION = 50000
+    EVALUATION_PERIOD = 5000
 
     """ Execution """
     print("Experiment name: %s" % EXPERIMENT_NAME)
-    train_continuous(PLAYER(config.BLACK, ValueFunction), PLAYER(config.WHITE, ValueFunction), ITERATIONS)
+    train_continuous(player1=PLAYER(config.BLACK, ValueFunction), player2=PLAYER(config.WHITE, ValueFunction), games=GAMES_PER_ITERATION, evaluation_period=EVALUATION_PERIOD, experiment_name=EXPERIMENT_NAME, iterations=ITERATIONS)
     print("Training completed")
