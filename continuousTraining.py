8d3dba70bb41f890fc0922c56341f12b71fee3cf
masus04 <thomas_st@hotmail.com>
2017-11-04 14:22:59 +0100

    Improved logging

--- continuousTraining.py
+++ continuousTraining.py
@@ -16,6 +16,7 @@ def train_continuous(player1, player2, games, evaluation_period, experiment_name
         train(player1, player2, games, evaluation_period, experiment_name)
         player1, player2 = (player1, player2) if compare_players(player1, player2, silent=(i != iterations-1)) >= 0 else (player2, player1)
         player2.value_function = player1.value_function.copy()
+        print("Simulation time: %s\n" % str(datetime.now()-start).split(".")[0])
 
     return player1, player2
 
@@ -29,6 +30,7 @@ if __name__ == "__main__":
     EVALUATION_PERIOD = 5000
 
     """ Execution """
+    start = datetime.now()
     print("Experiment name: %s" % EXPERIMENT_NAME)
     train_continuous(player1=PLAYER(config.BLACK, ValueFunction), player2=PLAYER(config.WHITE, ValueFunction), games=GAMES_PER_ITERATION, evaluation_period=EVALUATION_PERIOD, experiment_name=EXPERIMENT_NAME, iterations=ITERATIONS)
-    print("Training completed")
+    print("Training completed, took %s" % str(datetime.now()-start).split(".")[0])
