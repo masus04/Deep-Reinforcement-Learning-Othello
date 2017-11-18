6007594ce03b27fb1d8689298f195958909822e6
masus04 <thomas_st@hotmail.com>
2017-11-18 13:01:54 +0100

    Remove unnecessary file

--- convertWeightsToPlayers.py
+++
@@ -1,9 +0,0 @@
-import src.config as config
-import src.player as player
-
-pl1 = player.TDPlayer(config.BLACK)
-pl2 = player.TDPlayer(config.WHITE)
-
-for plr in (pl1, pl2):
-    plr.load_params()
-    plr.save()
