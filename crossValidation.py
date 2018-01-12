cec1c9f8524e46c958e246bcb38900994209af09
masus04 <thomas_st@hotmail.com>
2018-01-13 00:55:28 +0100

    Gridworld Crossvalidation Preparation

--- crossValidation.py
+++ crossValidation.py
@@ -18,11 +18,11 @@ def log_message(message):
     evaluation_file.write(message + "\n")
 
 
-def evaluation(games, evaluation_period, evaluation_games, e, a):
+def evaluation(games, evaluation_period, evaluation_games, lr, e, a):
     log_message("Evaluating epsilon:%s alpha:%s" % (e, a))
 
-    player1 = PLAYER(color=config.BLACK, strategy=ValueFunction, e=e, alpha=a)
-    player2 = PLAYER(color=config.WHITE, strategy=ValueFunction, e=e, alpha=a)
+    player1 = PLAYER(color=config.BLACK, strategy=ValueFunction, lr=lr, e=e, alpha=a)
+    player2 = PLAYER(color=config.WHITE, strategy=ValueFunction, lr=lr, e=e, alpha=a)
     players = [player1, player2]
 
     """ Training """
@@ -48,6 +48,7 @@ if __name__ == "__main__":
 
     start_time = datetime.now()
 
+    lr = 0.1
     epsilons = [0.001, 0.0003, 0.0001, 0.00003, 0.00001]
     alphas = [0.03, 0.01, 0.003, 0.001]
 
@@ -55,9 +56,9 @@ if __name__ == "__main__":
     EVALUATION_PERIOD = 1000    # How often the performance is evaluated
     EVALUATION_GAMES = 80       # Number of final evaluation games
 
-    results = [(evaluation(TRAINING_GAMES, EVALUATION_PERIOD, EVALUATION_GAMES, e, a), e, a) for e in epsilons for a in alphas]
-    for r in sorted(results):
-        log_message("\nscore:%s e:%s a:%s" % r)
+    results = [(evaluation(TRAINING_GAMES, EVALUATION_PERIOD, EVALUATION_GAMES, lr, e, a), lr, e, a) for e in epsilons for a in alphas]
+    for r in sorted(results, reverse=True):
+        log_message("score:%s lr:%s e:%s a:%s" % r)
 
-    print("CrossValidation timer: %s" % str(datetime.now() - start_time).split(".")[0])
+    print("\nCrossValidation timer: %s" % str(datetime.now() - start_time).split(".")[0])
     evaluation_file.close()
